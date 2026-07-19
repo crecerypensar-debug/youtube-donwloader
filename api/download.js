43c303c44b7307f90c067646ca2ba69fb29cd2cf
crecerypensar-debug <crecerypensar@gmail.com>
2026-07-19 16:21:29 -0500

    Add files via upload

--- api/download.js
+++ api/download.js
@@ -3,16 +3,18 @@ export default async function handler(req, res) {
     const API_KEY = 'd34fae19f9mshabcb085f9847622p12881cjsn960d55f8645b';
     const API_HOST = 'youtube-mp4-mp3-downloader.p.rapidapi.com';
 
-    // allowExtendedDuration=true permite procesar videos de larga duración
     const url = `https://${API_HOST}/api/v1/download?format=${format}&id=${id}&audioQuality=${audioQuality}&addInfo=false&allowExtendedDuration=true`;
 
     try {
         const response = await fetch(url, {
-            headers: { 'X-RapidAPI-Key': API_KEY, 'X-RapidAPI-Host': API_HOST }
+            headers: {
+                'X-RapidAPI-Key': API_KEY,
+                'X-RapidAPI-Host': API_HOST
+            }
         });
         const data = await response.json();
         res.status(200).json(data);
     } catch (error) {
-        res.status(500).json({ error: 'Error en servidor' });
+        res.status(500).json({ error: 'Error' });
     }
 }
