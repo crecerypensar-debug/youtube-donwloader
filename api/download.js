728cd13881fb4ca9535b1b1ef0f5cd37c590f6ec
crecerypensar-debug <crecerypensar@gmail.com>
2026-07-19 16:52:06 -0500

    Add files via upload

--- api/download.js
+++ api/download.js
@@ -3,7 +3,11 @@ export default async function handler(req, res) {
     const API_KEY = 'd34fae19f9mshabcb085f9847622p12881cjsn960d55f8645b';
     const API_HOST = 'youtube-mp4-mp3-downloader.p.rapidapi.com';
 
-    const url = `https://${API_HOST}/api/v1/download?format=${format}&id=${id}&audioQuality=${audioQuality}&addInfo=false&allowExtendedDuration=true`;
+    // REFUERZO: Forzamos que audioQuality sea un número y no un texto
+    const qualityNum = parseInt(audioQuality);
+
+    // Intentamos cambiar la estructura de la URL para ver si la API responde mejor
+    const url = `https://${API_HOST}/api/v1/download?format=${format}&id=${id}&audioQuality=${qualityNum}&addInfo=false&allowExtendedDuration=true`;
 
     try {
         const response = await fetch(url, {
